5213d096088cbe46f4f579caf22444fa8767d5c3
João Pedro Borges Santos <joaopedrobs_dev@outlook.com>
2023-02-06 17:03:14 -0300

    first commit

---
+++ src/MemoryGame.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import GameBoard from "./components/GameBoard";
+import GameOver from "./components/GameOver";
+import game from "./game/game"
+
+function MemoryGame() {
+
+    const [gameOver, setGameOver] = useState(false);
+    const [cards, setCards] = useState([])
+
+    useEffect(() => {
+        setCards(game.createCardsFromTechs());
+    }, [])
+
+    function handleFlip(card) {
+        game.flipCard(card.id, () => {
+            //gameOverCallback
+            setGameOver(true)
+        }, () => {
+            //noMatchCallback
+            setCards([...game.cards])
+        });
+        setCards([...game.cards])
+    }
+
+    function restart() {
+        setCards(game.createCardsFromTechs());
+        setGameOver(false);
+    }
+
+    return (
+        <div>
+            <GameBoard handleFlip={handleFlip} cards={cards}></GameBoard>
+            <GameOver show={gameOver} handleRestart={restart}></GameOver>
+        </div>
+    )
+}
+
+export default MemoryGame;
